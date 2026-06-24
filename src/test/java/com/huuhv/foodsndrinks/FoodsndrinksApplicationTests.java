20c31f93a88d8f95b2e6dc0d442759882a631f4e
huuhv-0372 <hoang.van.huu@sun-asterisk.com>
2026-06-24 14:00:40 +0700

    Init project Foods and Drinks

---
+++ src/test/java/com/huuhv/foodsndrinks/FoodsndrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huuhv.foodsndrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsndrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
